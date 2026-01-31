8eaf7ddd6aa4d935bb71d9bea46fe2ed18e8c33e
Jonathan <jonathanaclinton@gmail.com>
2026-01-31 14:50:48 -0800

    update controller name

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController: MonoBehaviour
 {
     [Serializable]
     public class Attack

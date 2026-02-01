6ed7a591ac62876839dd9ae66db19c31c0552ff9
Jonathan <jonathanaclinton@gmail.com>
2026-01-31 19:10:41 -0800

    player health bar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,15 +1,32 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Health Settings")]
     [Tooltip("Player Health")]
-    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private Slider healthSlider;
+    private int health;
+
+    [Header("Health Flash Settings")]
+    [Tooltip("Flash Color")]
+    [SerializeField] Color colorToGoTo;
+
+    [Tooltip("Flash Time")]
+    [SerializeField] float flashTime;
+
+    private Color startColor;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
+        healthSlider.value = 1f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
@@ -26,5 +43,14 @@ public class PlayerHealth : MonoBehaviour
             health = 0;
             Debug.Log("Player has died placeholder");
         }
+        StartCoroutine(FlashEffect());
+        healthSlider.value = (float)health / (float)maxHealth;
     }
+    IEnumerator FlashEffect()
+    {
+        spriteRenderer.color = colorToGoTo;
+        yield return new WaitForSeconds(flashTime);
+        spriteRenderer.color = startColor;
+    }
+
 }
